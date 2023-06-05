5c42f7086d6aa8fdf530eeea02c927ba6f049524
pHMiguel <aphonso99@gmail.com>
2023-06-05 12:59:17 -0300

    GitHub Pages Config

--- src/screens/MainScreen/Main.screen.tsx
+++ src/screens/MainScreen/Main.screen.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import * as S from "./Main.styles";
 import { Button } from "../../components";
+import { Container, Row, Col } from "react-bootstrap";
 
 function MainScreen() {
   const [countHome, setCountHome] = useState<number>(0);
@@ -49,112 +50,145 @@ function MainScreen() {
 
   return (
     <S.Container>
-      <S.Title>Truco Scoreboard.</S.Title>
-      <S.Scoreboard>
-        <S.ContentAreaHome>
-          <S.InputName
-            type="text"
-            onChange={(e) => setNameHome(e.target.value)}
-            onFocus={() => setIsHomeFocused(true)}
-            onBlur={() => setIsHomeFocused(false)}
-            value={nameHome}
-            maxLength={NAME_LIMIT}
-          />
-          <S.NameCounterWrapper>
-            {isHomeFocused && (
-              <S.NameCounter>{`${nameHome.length} / ${NAME_LIMIT}`}</S.NameCounter>
-            )}
-          </S.NameCounterWrapper>
-          <S.CounterHome>
-            <h1>{countHome}</h1>
-          </S.CounterHome>
-          <S.ButtonArea>
-            <Button
-              onClick={() =>
-                updateValue({ set: setCountHome, type: "plus", value: 3 })
-              }
-            >
-              + 3
-            </Button>
-            <Button
-              onClick={() =>
-                updateValue({ set: setCountHome, type: "plus", value: 1 })
-              }
-            >
-              +
-            </Button>
-            <Button
-              onClick={() =>
-                updateValue({ set: setCountHome, type: "minus", value: 1 })
-              }
-            >
-              -
-            </Button>
-            <Button
-              onClick={() =>
-                updateValue({ set: setCountHome, type: "minus", value: 3 })
-              }
-            >
-              - 3
-            </Button>
-          </S.ButtonArea>
-        </S.ContentAreaHome>
-        <S.Versus>
-          <h1>X</h1>
-        </S.Versus>
-        <S.ContentAreaAway>
-          <S.InputName
-            type="text"
-            onChange={(e) => setNameAway(e.target.value)}
-            onFocus={() => setIsAwayFocused(true)}
-            onBlur={() => setIsAwayFocused(false)}
-            value={nameAway}
-            maxLength={NAME_LIMIT}
-          />
-          <S.NameCounterWrapper>
-            {isAwayFocused && (
-              <S.NameCounter>{`${nameAway.length} / ${NAME_LIMIT}`}</S.NameCounter>
-            )}
-          </S.NameCounterWrapper>
-          <S.CounterAway>
-            <h1>{countAway}</h1>
-          </S.CounterAway>
-          <S.ButtonArea>
-            <Button
-              onClick={() =>
-                updateValue({ set: setCountAway, type: "plus", value: 3 })
-              }
-            >
-              + 3
-            </Button>
-            <Button
-              onClick={() =>
-                updateValue({ set: setCountAway, type: "plus", value: 1 })
-              }
-            >
-              +
-            </Button>
-            <Button
-              onClick={() =>
-                updateValue({ set: setCountAway, type: "minus", value: 1 })
-              }
-            >
-              -
-            </Button>
-            <Button
-              onClick={() =>
-                updateValue({ set: setCountAway, type: "minus", value: 3 })
-              }
-            >
-              - 3
-            </Button>
-          </S.ButtonArea>
-        </S.ContentAreaAway>
-      </S.Scoreboard>
-      <S.ResetButtonArea>
-        <Button onClick={() => resetValue()}> Reset</Button>
-      </S.ResetButtonArea>
-      <S.Credit>pH. / A e s t a s - MaathAsul / iVanittas</S.Credit>
+      <Container fluid>
+        <Row>
+          <S.Title>Truco Scoreboard.</S.Title>
+        </Row>
+        <Row>
+          <S.Scoreboard>
+            <Col sm>
+              <S.ContentAreaHome>
+                <S.InputName
+                  type="text"
+                  onChange={(e) => setNameHome(e.target.value)}
+                  onFocus={() => setIsHomeFocused(true)}
+                  onBlur={() => setIsHomeFocused(false)}
+                  value={nameHome}
+                  maxLength={NAME_LIMIT}
+                />
+                <S.NameCounterWrapper>
+                  {isHomeFocused && (
+                    <S.NameCounter>{`${nameHome.length} / ${NAME_LIMIT}`}</S.NameCounter>
+                  )}
+                </S.NameCounterWrapper>
+                <S.CounterHome>
+                  <h1>{countHome}</h1>
+                </S.CounterHome>
+                <S.ButtonArea>
+                  <Button
+                    onClick={() =>
+                      updateValue({ set: setCountHome, type: "plus", value: 3 })
+                    }
+                  >
+                    + 3
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      updateValue({ set: setCountHome, type: "plus", value: 1 })
+                    }
+                  >
+                    +
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      updateValue({
+                        set: setCountHome,
+                        type: "minus",
+                        value: 1,
+                      })
+                    }
+                  >
+                    -
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      updateValue({
+                        set: setCountHome,
+                        type: "minus",
+                        value: 3,
+                      })
+                    }
+                  >
+                    - 3
+                  </Button>
+                </S.ButtonArea>
+              </S.ContentAreaHome>
+            </Col>
+            <Col sm>
+              <S.Versus>
+                <h1>X</h1>
+              </S.Versus>
+            </Col>
+            <Col sm>
+              <S.ContentAreaAway>
+                <S.InputName
+                  type="text"
+                  onChange={(e) => setNameAway(e.target.value)}
+                  onFocus={() => setIsAwayFocused(true)}
+                  onBlur={() => setIsAwayFocused(false)}
+                  value={nameAway}
+                  maxLength={NAME_LIMIT}
+                />
+                <S.NameCounterWrapper>
+                  {isAwayFocused && (
+                    <S.NameCounter>{`${nameAway.length} / ${NAME_LIMIT}`}</S.NameCounter>
+                  )}
+                </S.NameCounterWrapper>
+                <S.CounterAway>
+                  <h1>{countAway}</h1>
+                </S.CounterAway>
+                <S.ButtonArea>
+                  <Button
+                    onClick={() =>
+                      updateValue({ set: setCountAway, type: "plus", value: 3 })
+                    }
+                  >
+                    + 3
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      updateValue({ set: setCountAway, type: "plus", value: 1 })
+                    }
+                  >
+                    +
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      updateValue({
+                        set: setCountAway,
+                        type: "minus",
+                        value: 1,
+                      })
+                    }
+                  >
+                    -
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      updateValue({
+                        set: setCountAway,
+                        type: "minus",
+                        value: 3,
+                      })
+                    }
+                  >
+                    - 3
+                  </Button>
+                </S.ButtonArea>
+              </S.ContentAreaAway>
+            </Col>
+          </S.Scoreboard>
+        </Row>
+
+        <Row>
+          <Col>
+            <S.ResetButtonArea>
+              <Button onClick={() => resetValue()}> Reset</Button>
+            </S.ResetButtonArea>
+          </Col>
+        </Row>
+        <S.Credit>pH. / A e s t a s - MaathAsul / iVanittas</S.Credit>
+      </Container>
     </S.Container>
   );
 }
--- src/screens/MainScreen/Main.styles.ts
+++ src/screens/MainScreen/Main.styles.ts
@@ -27,22 +27,18 @@ export const Scoreboard = styled.div`
 
 export const ContentAreaHome = styled.div`
   font-size: 1em;
-  width: 600px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 20px;
 `;
 
 export const ContentAreaAway = styled.div`
   font-size: 1em;
-  width: 600px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 20px;
 `;
 
 export const CounterHome = styled.div`
